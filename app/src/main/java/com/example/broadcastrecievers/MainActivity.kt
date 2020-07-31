7980c41a120f18cbee30748c944aecf943a65e6c
anorovhasan <60793393+anorovhasan@users.noreply.github.com>
2020-07-31 10:31:24 +0500

    Share button added in the toolbar

--- app/src/main/java/com/example/broadcastrecievers/MainActivity.kt
+++ app/src/main/java/com/example/broadcastrecievers/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.broadcastrecievers
 
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
             registerReceiver(receiver, it)
         }
+
     }
 
     override fun onStop() {
@@ -25,4 +30,26 @@ class MainActivity : AppCompatActivity() {
         unregisterReceiver(receiver)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_share,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share ->{
+                val intent=Intent(Intent.ACTION_SEND)
+                intent.type = "text/plan"
+                var sharingBody ="Your body here"
+                var shareSubject ="Your Subject here"
+
+                intent.putExtra(Intent.EXTRA_TEXT,sharingBody)
+                intent.putExtra(Intent.EXTRA_SUBJECT,shareSubject)
+
+                startActivity(Intent.createChooser(intent,"Share Using "))
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
